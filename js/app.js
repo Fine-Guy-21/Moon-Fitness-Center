35d8be0d1c617be58874e0f6bad9566c31867ace
Fine Guy <122565078+Fine-Guy-21@users.noreply.github.com>
2025-07-16 11:01:31 +0300

    Update CSS variables for light theme, adjust hero section styles, and implement theme toggle functionality

--- js/app.js
+++ js/app.js
@@ -14,16 +14,26 @@ document.addEventListener('click', (e) => {
     }
 });
 
+
+
 // Header transparency on scroll
 window.addEventListener('scroll', () => {
-    // console.log(window.scrollY);
     const header = document.querySelector('.header');
+
+    // Get the computed style of the root element
+    const rootStyles = getComputedStyle(document.documentElement);
+    
+    // Get the CSS variables
+    const stickyBg = rootStyles.getPropertyValue('--header-bg').trim();
+    const headerText = rootStyles.getPropertyValue('--white')
+
     if (window.scrollY > 50) {
-        header.style.background = 'rgba(26, 26, 26, 0.95)';
+        header.style.background = stickyBg; // Use the sticky background color
     } else {
-        header.style.background = 'none';
+        header.style.background = 'none'; // Use the transparent background
     }
 });
+
 
 // Section 2
 
